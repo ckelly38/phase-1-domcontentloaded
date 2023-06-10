15f6c24e15226ebbfdb2a0440ec75e5a236956cf
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-06-10 01:59:53 -0600

    DOMContentLoaded event and event example

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    //console.log("the DOM has loaded!");
+    document.getElementById("text").textContent = "This is really cool!";
+});
+//console.log("before the DOM loads fully!");
